c95bf05c40cd3857f8ed9eb4cfd0d1d14d8c81ba
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2015-11-15 18:08:27 +0800

    Fix wrong java path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 1337;
 
 // Azure path
-var javaExec = "\"D:/Program Files (x86)/Java/jdk1.8.0_60/java\"";
+var javaExec = "\"D:/Program Files/Java/jdk1.8.0_60/java\"";
 
 // Local path
 if (port == 1337) {

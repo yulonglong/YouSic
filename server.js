fd2231bed4a7727afbc0ce3f9b12dec0d2eda783
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2015-11-15 16:47:16 +0800

    Modified port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 1337;
 
 app.use(express.static(__dirname + '/public_html'));
 
@@ -23,8 +24,8 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(port, function(){
+	console.log('listening on *:'+port);
 });
 
 
